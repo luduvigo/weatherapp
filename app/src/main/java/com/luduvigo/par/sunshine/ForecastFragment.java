6a7c9f75145e3f80651ee7f093f7a5778e51f97e
luduvigo <paoloantoniorossi@gmail.com>
2015-05-17 23:35:57 +0200

    Move networking code to AsyncTask

---
+++ app/src/main/java/com/luduvigo/par/sunshine/ForecastFragment.java
@@ -0,0 +1,128 @@
+package com.luduvigo.par.sunshine;
+
+/**
+ * Created by par on 17/05/15.
+ */
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A placeholder fragment containing a simple view
+ */
+public class ForecastFragment extends Fragment {
+
+    public ForecastFragment(){
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        //Create some dummy data for the ListView. Here's a sample weekly forecast
+        String[] data = {
+                "Mon 6/23 - Paolo - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        };
+
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+
+        //The ArrayAdapter will take from a source and
+        //use it to populate a ListView it's attached to.
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<String>(
+                        //The current context (this fragment parent activity)
+                        getActivity(),
+                        //Id of the list item layout
+                        R.layout.list_item_forecast,
+                        //Id of the textview to populate
+                        R.id.list_item_forecast_textview,
+                        //Forecast data
+                        weekForecast);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(arrayAdapter);
+
+        return rootView;
+    }
+
+    public class FetchWeatherTask extends AsyncTask<Void, Void, Void>{
+
+        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
+
+        protected Void doInBackground(Void... params){
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            //Will contain the raw JSON response as a string
+            String forecastJsonStr = null;
+
+            try {
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+
+                //Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                //Read the response into a string
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if(inputStream == null){
+                    //Nothing to do
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while((line = reader.readLine()) != null){
+                    buffer.append(line + "\n");
+                }
+
+                if(buffer.length() == 0){
+                    return null;
+                }
+                forecastJsonStr = buffer.toString();
+
+
+            } catch (IOException e){
+                Log.e("PlaceholderFragment", "Error", e);
+                return null;
+            } finally {
+                if(urlConnection != null)
+                    urlConnection.disconnect();
+                if(reader != null){
+                    try {
+                        reader.close();
+                    } catch (final IOException e){
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}

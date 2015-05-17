f2b63a9ff6ac0cdd2ccd5526efdec84286080e1c
luduvigo <paoloantoniorossi@gmail.com>
2015-05-17 21:48:47 +0200

    Add networking code

--- app/src/main/java/com/luduvigo/par/sunshine/MainActivity.java
+++ app/src/main/java/com/luduvigo/par/sunshine/MainActivity.java
@@ -71,6 +71,56 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState){
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            //Will contain the raw JSON response as a string
+            String forecastJsonStr = null;
+
+            try {
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+
+                //Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                //Read the response into a string
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if(inputStream == null){
+                    //Nothing to do
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while((line = reader.readLine()) != null){
+                    buffer.append(line + "\n");
+                }
+
+                if(buffer.length() == 0){
+                    return null;
+                }
+                forecastJsonStr = buffer.toString();
+
+
+            } catch (IOException e){
+                Log.e("PlaceholderFragment", "Error", e);
+                return null;
+            } finally {
+                 if(urlConnection != null)
+                     urlConnection.disconnect();
+                 if(reader != null){
+                     try {
+                         reader.close();
+                     } catch (final IOException e){
+                         Log.e("PlaceholderFragment", "Error closing stream", e);
+                     }
+                 }
+            }
+
             //Create some dummy data for the ListView. Here's a sample weekly forecast
             String[] data = {
                     "Mon 6/23 - Sunny - 31/17",

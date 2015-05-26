cf951dfbe959718c8575763bd3f6576caf881aa8
luduvigo <paoloantoniorossi@gmail.com>
2015-05-26 22:58:01 +0200

    Fix dependencies to run tests

--- app/src/androidTest/java/com/luduvigo/par/sunshine/data/TestPractice.java
+++ app/src/androidTest/java/com/luduvigo/par/sunshine/data/TestPractice.java
@@ -1,4 +1,4 @@
-package com.example.android.sunshine.app.data;
+package com.luduvigo.par.sunshine.data;
 
 import android.test.AndroidTestCase;
 
--- app/src/androidTest/java/com/luduvigo/par/sunshine/utils/PollingCheck.java
+++ app/src/androidTest/java/com/luduvigo/par/sunshine/utils/PollingCheck.java
@@ -15,7 +15,7 @@
  *
  * Note: This file copied from the Android CTS Tests
  */
-package com.example.android.sunshine.app.utils;
+package com.luduvigo.par.sunshine.utils;
 
 import junit.framework.Assert;
 
